6ea99de32201622e66b51489bdc640c23377108b
DarkOnion0 <darkonion0@clapot.org>
2023-10-10 18:50:14 +0200

    feat(titration/titrageConductimetrique): add the equivalence point

--- src/titration/main.py
+++ src/titration/main.py
@@ -89,12 +89,13 @@ def titragePH(title, x, y, showVeLine: bool = False):
     ax.legend()
 
 
-def titrageConductimetrique(title, x, y, showVeLine: bool = False):
+def titrageConductimetrique(title, x, y, split, showVeLine: bool = False):
     """
     Créer un graphique d'analyse de titrage conductimétrique en fonctions des paramètres suivants :
     - title : le titre du graphique
     - x : l'axe des abscisse [nom, coordonées]
     - y : l'axe des ordonnées [nom, coordonées]
+    - split : l'indexe python de ou rompre la série de donnée
     - showVeLine: Affiche la droite verticale corréspondant aux volume à l'équivalence
     """
 
@@ -114,9 +115,8 @@ def titrageConductimetrique(title, x, y, showVeLine: bool = False):
     ######################
 
     # Find the bottom of the V shape dataset
-    (split,) = np.where(y[1] == min(y[1]))
-
-    print(split[0])
+    # split, = np.where(y[1] == min(y[1]))
+    split = [split]
 
     # Split x
     x1 = x[1][: split[0] + 1]
@@ -136,29 +136,44 @@ def titrageConductimetrique(title, x, y, showVeLine: bool = False):
     m1, p1, r1, _, _ = sp.stats.linregress(x1, y1)
     m2, p2, r2, _, _ = sp.stats.linregress(x2, y2)
 
+    ############################
+    ## Find line intersection ##
+    ############################
+
+    x_point = (p1 - p2) / (m2 - m1)
+    y_point = m1 * x_point + p1
+
+    ax.annotate(
+        f"Equivalence $(x = {round(x_point, 2)}; \ y = {round(y_point, 2)})$",
+        xy=(x_point, y_point),
+        xytext=(x_point * 1.05, y_point * 0.50),
+        # xytext=(x_point * 0.7, y_point * 1.15),
+        arrowprops=dict(arrowstyle="->", connectionstyle="arc,angleA=0,armA=50,rad=10"),
+    )
+
     ##############
     ## Plotting ##
     ##############
 
     # Original point
-    plt.scatter(x[1], y[1], marker="o", color="chocolate", label="Experimental points")
+    plt.scatter(x[1], y[1], marker="o", color="chocolate", label="Points expérimentaux")
 
     # Regressions
     ax.plot(
         x1_new,
         m1 * x1_new + p1,
         color="mediumspringgreen",
-        label=f"Linear regression 1 ($R^2$ = {round(r1**2, 4)})",
+        label=f"Régression linéaire 1 ($R^2$ = {round(r1**2, 4)})",
     )
     ax.plot(
         x2_new,
         m2 * x2_new + p2,
         color="mediumslateblue",
-        label=f"Linear regression 2 ($R^2$ = {round(r2**2, 4)})",
+        label=f"Régression linéaire 2 ($R^2$ = {round(r2**2, 4)})",
     )
 
     # Horizontal line of the V_e
-    # if showVeLine:
-    #    plt.axvline(x_point, linestyle="--", color="palegreen", label="$V_e$")
+    if showVeLine:
+        plt.axvline(x_point, linestyle="--", color="olive", label="$V_e$")
 
     ax.legend()
